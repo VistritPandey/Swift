e0d7dfcf715bb58e3c9ec8d2bd825edfbad11864
Vistrit <vistritpandey@gmail.com>
2021-02-25 10:29:30 +0530

    initial

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Vistrit Pandey on 25/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, test!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Shared
+//
+//  Created by Vistrit Pandey on 25/02/21.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
